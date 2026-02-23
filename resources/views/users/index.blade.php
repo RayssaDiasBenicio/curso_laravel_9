c0e3c4c240c73cca1eadc8f46bd47f3241b1268a
Rayssa <rayssadiasbenicio@gmail.com>
2026-02-23 10:04:13 -0300

    teste merge

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,22 +3,22 @@
 @section('title', 'Listagem dos Usuários')
 
 @section('content')
-
 <h1 class="text-2x1 font-semibold leading-tigh py-2">
     Listagem dos Usuários
-    (<a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>)
+    <a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
 </h1>
 
 <form action="{{ route('users.index') }}" method="get">
     <input type="text" name="search" placeholder="Pesquisar">
     <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white px-4 text-sm">Pesquisar</button>
 </form>
-<table class="min-w-full divide-y divide-gray-200 overflow-x-auto">
-    <thead class="bg-gray-50">
+
+<table class="min-w-full leadind-normal shadow-md rounded-lg overflow-hidden">
+    <thead">
         <tr>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">E-mail</th>
-            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Ações</th>
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Nome</th>
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">E-mail</th>
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Ações</th>
         </tr>
     </thead>
     <tbody class="bg-white divide-y divide-gray-200">
